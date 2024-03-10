4da16e0b8ecb7d2665cb4c4f709d2800380e8c08
bartvanhoey <bartvanhoey@hotmail.com>
2024-03-10 14:55:17 +0100

    Update

---
+++ CustomizeControlsApp/CustomizeControlsApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomizeControlsApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CustomizeControlsApp/CustomizeControlsApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CustomizeControlsApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
