8ebf81d1261fe6e632f0c82ba1187c796bf53dd3
bartvanhoey <bartvanhoey@hotmail.com>
2024-03-26 16:26:38 +0100

    Update

---
+++ SnackBarToastApp/SnackbarToastApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SnackbarToastApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SnackBarToastApp/SnackbarToastApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace SnackbarToastApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            string message;
+            
+            if (count == 1)
+                message = $"Clicked {count} time";
+            else
+                message = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(message);
+
+            CounterBtn.Text = message;
+
+            // Toast.Make(message, ToastDuration.Long, 30).Show();
+
+            var snackbar = Snackbar.Make(message, () => DisplayAlert("Did you subsribe", "to my chanel yet?", "OK"), "YES!",
+                TimeSpan.FromSeconds(10),
+                new SnackbarOptions
+                {
+                    BackgroundColor = Colors.Red,
+                    TextColor = Colors.White
+                });
+
+            snackbar.Show();
+        }
+    }
+
+}
